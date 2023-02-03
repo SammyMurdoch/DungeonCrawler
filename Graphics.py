ffcacafa589b69fd8cce612a6b3be26a70409916
Sammy Murdoch <sammy.murdoch.0@gmail.com>
2023-02-03 10:08:43 +0000

    Fixed the bug where not all the tiles were displaying and changed the look a bit

--- Graphics.py
+++ Graphics.py
@@ -11,7 +11,7 @@ class Graphics:
         dungeon_surf = pygame.display.set_mode((3840, 2400), pygame.FULLSCREEN)
 
         rows = 10
-        columns = int((rows/5)*8)
+        columns = int((rows/5)*8)  # TODO not used but could be needed to set bounds on coordinates
 
         square_size = dungeon_surf.get_height()//rows
 
@@ -41,27 +41,23 @@ class Graphics:
                         if (player.coords[0] - 1, player.coords[1]) in self.display_tiles:
                             player.coords[0] -= 1
 
-            dungeon_surf.fill((0, 0, 0))
-
+            dungeon_surf.fill((128, 128, 128))
 
             player_rect.center = ((player.coords[0] + 1/2) * square_size, (player.coords[1] + 1/2) * square_size)  # TODO make this a function
-            for row in range(rows):
-                for col in range(columns):
-                    if (row, col) in self.display_tiles:
-                        if (row+col) % 2:
-                            colour = (100, 0, 0)
-                        else:
-                            colour = (200, 100, 0)
 
-                        square = pygame.Rect(row * square_size, col * square_size, square_size, square_size)
-                        pygame.draw.rect(dungeon_surf, colour, square)
+            for tile in self.display_tiles:
+                tile_surf = pygame.image.load("tile_hatch.png").convert_alpha()
+                tile_rect = tile_surf.get_rect()
+                tile_rect.topleft = (tile[0]*square_size, tile[1]*square_size)
+                dungeon_surf.blit(tile_surf, tile_rect)
 
             dungeon_surf.blit(player_surf, player_rect)
 
             pygame.display.update()
 
 hi = Graphics([(0, 0), (0, 1), (1, 1), (1, 2), (1, 3), (1, 4), (2, 4), (2, 5), (3, 5), (4, 5), (2, 2), (3, 2), (4, 2),
-               (4, 3), (4, 4)])
+               (4, 3), (4, 4), (5, 4), (6, 4), (7, 4), (8, 4), (9, 4), (10, 4), (9, 5), (9, 6), (10, 6), (11, 6), (12, 6),
+               (13, 6), (13, 7), (13, 8), (12, 7), (12, 8), (14, 8), (15, 8), (11, 4), (12, 4), (12, 5)])
 
 hi.display_graphics()
 
