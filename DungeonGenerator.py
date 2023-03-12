06ce547c557345cbe4d540dae03fd463a39a5b4a
Sammy Murdoch <sammy.murdoch.0@gmail.com>
2023-03-12 17:28:04 +0000

    Fixed some bugs... hopefully...

--- DungeonGenerator.py
+++ DungeonGenerator.py
@@ -6,16 +6,17 @@ import math
 
 np.set_printoptions(threshold=np.inf)
 
+
 class TreeNode:
-    def __init__(self, parent=None, children=None):
+    def __init__(self, parent: int=None, children: list[int]=None) -> None:
         self.index = None
         self.parent_index = parent
         self.children_indices = children
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Parent: {self.parent_index}, Children: {self.children_indices}'
 
-    def add_child(self, child):
+    def add_child(self, child: int) -> None:
         if self.children_indices is None:
             self.children_indices = {child}
         else:
@@ -23,14 +24,14 @@ class TreeNode:
 
 
 class Tree:
-    def __init__(self, root=None):
+    def __init__(self, root: TreeNode=None) -> None:
         self.nodes = {}
         self.root = None
 
         if root is not None:
             self.add_node(root)
 
-    def add_node(self, tree_node: TreeNode):
+    def add_node(self, tree_node: TreeNode) -> None:
         tree_node.index = len(self)
 
         if tree_node.parent_index is None:
@@ -47,23 +48,24 @@ class Tree:
             else:
                 raise ValueError("Parent not in the tree.")
 
-    def __len__(self):
-        if self.nodes is None:
+    def __len__(self) -> int:
+        try:
+            return len(self.nodes)
+        except (AttributeError, TypeError):
             return 0
 
-        return len(self.nodes)
-
-
 class PartitionTree(Tree):
-    def __init__(self, root=None):
-        self.end_nodes = None
-        self.active_end_nodes = None
+    def __init__(self, root=None) -> None:
+        if root is not None:
+            self.end_nodes = {len(self)}
+            self.active_end_nodes = {len(self)}
+        else:
+            self.end_nodes = None
+            self.active_end_nodes = None
 
         super().__init__(root)
 
-
-
-    def __str__(self):
+    def __str__(self) -> str:
         s = ""
 
         for node in self.nodes:
@@ -73,7 +75,7 @@ class PartitionTree(Tree):
 
         return s
 
-    def add_node(self, node: TreeNode):
+    def add_node(self, node: TreeNode) -> None:
         super().add_node(node)
 
         if self.end_nodes is None:
@@ -88,7 +90,7 @@ class PartitionTree(Tree):
                 self.active_end_nodes.remove(node.parent_index)
 
     @property
-    def is_complete(self):
+    def is_complete(self) -> bool:
         if self.active_end_nodes is None:
             return False
 
@@ -99,63 +101,48 @@ class PartitionTree(Tree):
 
 
 class PartitionNode(TreeNode):
-    def __init__(self, bounds, parent=None, room=None):
+    def __init__(self, bounds: list[list[int]], parent: int=None, room: list[list[int]]=None) -> None:
         super().__init__(parent)
 
         self.bounds = bounds
         self.room = room
 
-    def __str__(self): # TODO change to print out all attributes in a certain list
+    def __str__(self) -> str: # TODO change to print out all attributes in a certain list
         return f'Bounds: {self.bounds}, Room: {self.room}, {super().__str__()}'
 
     @property
-    def x_len(self):
+    def x_len(self) -> float:
         return abs(self.bounds[1][0] - self.bounds[0][0])
 
     @property
-    def y_len(self):
+    def y_len(self) -> float:
         return abs(self.bounds[1][1] - self.bounds[0][1])
 
     @property
-    def is_indivisible(self):
+    def is_indivisible(self) -> bool:
         if (self.x_len < 5) or (self.y_len < 5):
             return True
         else:
             return False
 
 
-tree = PartitionTree(PartitionNode(None))
-tree.add_node(PartitionNode(None, 0))
-tree.add_node(PartitionNode(None, 0))
-tree.add_node(PartitionNode(None, 1))
-tree.add_node(PartitionNode(None, 3))
-tree.add_node(PartitionNode(None, 4))
-
-print(tree)
-print("\n")
-
-
-
-
 class Dungeon:
-    def __init__(self, bounds):
+    def __init__(self, bounds: list[list[int]]):
         self.dungeon_tree = PartitionTree(PartitionNode(bounds))
-        self.dungeon_tree.add_node()
 
         while not self.dungeon_tree.is_complete:
             for partition in self.dungeon_tree.active_end_nodes.copy():
-                Dungeon.partition_partition(self, partition)
+                Dungeon.partition_partition(self, self.dungeon_tree.nodes[partition])
+
+        print(self.dungeon_tree)
 
         for zone in self.dungeon_tree.end_nodes:
-            print(f'Zone: {self.dungeon_tree.nodes[zone].bounds}')
             zone_object = self.dungeon_tree.nodes[zone]
+
+            print(f'Zone: {zone_object.bounds}')
 
             # length = randint(2, self.dungeon_tree.nodes[zone].x_len)
             # height = randint(2, self.dungeon_tree.nodes[zone].y_len)
-
-            length = self.dungeon_tree.nodes[zone].x_len
-            height = self.dungeon_tree.nodes[zone].y_len
-
 
 
             # b_l_x = randint(zone_object.bounds[0][0], zone_object.bounds[1][0] - length)
@@ -167,7 +154,7 @@ class Dungeon:
 
 
             # zone_object.room = [(b_l_x, b_l_y), (b_l_x+length-1, b_l_y+height-1)] # TODO put back?
-            zone_object.room = [(b_l_x, b_l_y), (b_l_x+length, b_l_y+height)]
+            zone_object.room = [(b_l_x, b_l_y), (b_l_x+zone_object.x_len, b_l_y+zone_object.y_len)]
 
 
         self.dungeon_matrix = np.zeros((bounds[1][1], bounds[1][0]))
@@ -194,12 +181,12 @@ class Dungeon:
             zone_object = self.dungeon_tree.nodes[zone]  # TODO this should just be an attribute
             r = zone_object.room
 
-            self.dungeon_matrix[r[0][1]: r[1][1] + 1, r[0][0]: r[1][0] + 1] = 1 # Flipped due to matrix geometry
-            colour_mat[r[0][1]: r[1][1] + 1, r[0][0]: r[1][0] + 1] = zone
+            self.dungeon_matrix[r[0][1]: r[1][1], r[0][0]: r[1][0]] = 1 # Flipped due to matrix geometry
+            colour_mat[r[0][1]: r[1][1], r[0][0]: r[1][0]] = zone
 
             print(zone_object.room, "hi")
 
-        print(self.dungeon_matrix)
+        #print(self.dungeon_matrix)
 
         print()
 
@@ -209,9 +196,7 @@ class Dungeon:
         plt.show()
 
 
-    def partition_partition(self, partition):
-        x_len = self.dungeon_tree.nodes[partition].x_len
-        y_len = self.dungeon_tree.nodes[partition].y_len
+    def partition_partition(self, partition: PartitionNode):
         # if self.dungeon_tree.nodes[partition].x_len < 5: maybe put this back and make splitting based on a probability distribution
         #     Dungeon.split(self, 1, partition)
         # elif self.dungeon_tree.nodes[partition].y_len < 5:
@@ -219,14 +204,16 @@ class Dungeon:
         # else:
         #     Dungeon.split(self, randint(0, 1), partition)
 
-        if x_len >= 5 and y_len >= 5:
-            if Dungeon.random_split((x_len, y_len)):
-                Dungeon.split(self, randint(0, 1), partition)
+        if partition.x_len >= 5 and partition.y_len >= 5:
+            if Dungeon.random_split((partition.x_len, partition.y_len)):
+                Dungeon.split_partition(self, randint(0, 1), partition)
             else:
-                self.dungeon_tree.active_end_nodes.remove(partition)
+                self.dungeon_tree.active_end_nodes.remove(partition.index)
+        else:
+            self.dungeon_tree.active_end_nodes.remove(partition.index)
 
     @staticmethod
-    def random_split(dim, min_a=4, max_a=100):  # TODO if outside of the bounds, return no split, might need to change the other bit that decides on the split
+    def random_split(dim: list, min_a: int=4, max_a: int=400):  # TODO if outside of the bounds, return no split, might need to change the other bit that decides on the split
         p_f = lambda x: (x-min_a)/(max_a-min_a)
         probability = p_f(math.prod(dim))
 
@@ -235,8 +222,8 @@ class Dungeon:
 
         return False
 
-    def split(self, direction, partition):
-        initial_bounds = self.dungeon_tree.nodes[partition].bounds
+    def split_partition(self, direction: int, partition: PartitionNode):
+        initial_bounds = partition.bounds
         l_b = initial_bounds[0][direction] + 2
         u_b = initial_bounds[1][direction] - 2
 
@@ -251,72 +238,9 @@ class Dungeon:
         sub_par_2_b[0][direction] = split_point
         sub_par_2_b[0][(direction+1) % 2] = initial_bounds[0][(direction+1) % 2]
 
-        self.dungeon_tree.add_node(sub_par_1_b, partition)
-        self.dungeon_tree.add_node(sub_par_2_b, partition)
-
-# class PartitionTree(Tree):
-#     def __init__(self, root):
-#         root_id = 0
-#         self.nodes = {root_id: PartitionNode(root, None)}
-#         self.end_nodes = {root_id}
-#         self.active_end_nodes = {root_id}
-#
-#
-#     def add_node(self, value, parent):
-#         node_index = len(self.nodes)
-#         self.nodes[node_index] = PartitionNode(value, parent)
-#
-#         if parent is not None:
-#             self.nodes[parent].add_child(node_index)
-#             if parent in self.end_nodes:
-#                 self.end_nodes.remove(parent)
-#                 self.active_end_nodes.remove(parent)
-#
-#         if not self.nodes[node_index].is_indivisible:
-#             self.active_end_nodes.add(node_index)
-#
-#         self.end_nodes.add(node_index)
-#
-#     def __len__(self):
-#         return len(self.nodes)
-#
-#     @property
-#     def is_complete(self):
-#         if not len(self.active_end_nodes):
-#             return True
-#
-#         return False
-#
-#
-# class PartitionNode:
-#     def __init__(self, bounds, parent):
-#         self.bounds = bounds
-#         self.parent = parent
-#         self.children = []
-#         self.room = None
-#
-#     def add_child(self, child):
-#         self.children.append(child)
-#
-#     def __str__(self):
-#         return f'Bounds: {self.bounds}\nParent: {self.parent}\nChildren: {self.children}\n'
-#
-#     @property
-#     def x_len(self):
-#         return self.bounds[1][0] - self.bounds[0][0]
-#
-#     @property
-#     def y_len(self):
-#         return self.bounds[1][1] - self.bounds[0][1]
-#
-#     @property
-#     def is_indivisible(self):
-#         if self.x_len < 5 or self.y_len < 5:
-#             return True
-#         else:
-#             return False
+        self.dungeon_tree.add_node(PartitionNode(sub_par_1_b, partition.index))
+        self.dungeon_tree.add_node(PartitionNode(sub_par_2_b, partition.index))
 
 
-#
-# hi = Dungeon(([0, 0], [20, 20]))
+hi = Dungeon(([0, 0], [100, 100]))
 
