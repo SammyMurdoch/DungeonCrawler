2d06c8b526ac76b2ab77360df3c7bab6a8db919f
SammyMurdoch <116684824+SammyMurdoch@users.noreply.github.com>
2023-03-25 00:01:39 +0000

    Delete NPC Name Generator.py

--- NPC Name Generator.py
+++
@@ -1,8 +0,0 @@
-import os
-import openai
-
-openai.organization = "org-4XGM030xSxlU7dUaOFF2DXX8"
-openai.api_key = os.getenv("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")
-
-openai.Model.list()
-
