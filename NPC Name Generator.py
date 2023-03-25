2e67a32f2a1836e754f6a29e3986f3eeaf5c6785
Sammy Murdoch <sammy.murdoch.0@gmail.com>
2023-03-25 00:04:01 +0000

    Openai key fix

--- NPC Name Generator.py
+++ NPC Name Generator.py
@@ -1,8 +1,11 @@
 import os
 import openai
 
+with open("OpenAIAPIKey") as f:
+    key = f.read()
+
 openai.organization = "org-4XGM030xSxlU7dUaOFF2DXX8"
-openai.api_key = os.getenv("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")
+openai.api_key = os.getenv(key)
 
 openai.Model.list()
 
